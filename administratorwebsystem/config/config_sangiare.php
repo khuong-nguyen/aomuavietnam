b7e3a0dffd42406d18f6e16c6710d85449970ce9
khuong-nguyen <khuongnv@nexlesoft.com>
2021-04-05 08:52:44 +0700

    SEO

--- administratorwebsystem/config/config_sangiare.php
+++ administratorwebsystem/config/config_sangiare.php
@@ -6,16 +6,8 @@
 $config['site_name_vi'] = 'Công Ty TNHH Áo Mưa Minh Nguyên';
 $config['site_close'] = 0;
 $config['site_message_close_vi'] = 'Website đang trong quá trình nâng cấp. Vui lòng truy cập lại sau';
-$config['site_des_vi'] = 'Chuyên sản xuất áo mưa. Áo mưa thời trang, áo mưa Covic 19, áo đi mưa, áo mưa quảng cáo, Áo mưa tặng phẩm
-Áo mưa nhựa cao cấp,
-Áo mưa vải dù cao cấp,
-Áo mưa cánh dơi,
-Áo mưa bán lẻ,
-Áo mưa trẻ em,
-Áo mưa bộ vải dù,
-Áo mưa kiểu,
-Áo mưa bít,
-Áo mưa hai đầu';
+$config['site_des_vi'] = 'Chuyên sản xuất áo mưa. Áo mưa thời trang, áo mưa Covic 19, áo đi mưa, áo mưa quảng cáo, Áo mưa tặng phẩm, Áo mưa nhựa cao cấp,';
+$config['site_des_vi'] = $config['site_des_vi']. ' Áo mưa vải dù cao cấp, Áo mưa cánh dơi, Áo mưa bán lẻ, Áo mưa trẻ em, Áo mưa bộ vải dù, Áo mưa kiểu, Áo mưa bít, Áo mưa hai đầu';
 $config['site_keyword_vi'] = 'Áo mưa thời trang, áo mưa Covic 19, áo đi mưa, áo mưa quảng cáo, Áo mưa tặng phẩm, Áo mưa nhựa cao cấp, Áo mưa vải dù cao cấp,';
 $config['site_keyword_vi'] = $config['site_keyword_vi'] .' Áo mưa cánh dơi, Áo mưa bán lẻ, Áo mưa trẻ em, Áo mưa bộ vải dù, Áo mưa kiểu, Áo mưa bít, Áo mưa hai đầu';
 $config['site_name_en'] = 'Công Ty TNHH Áo Mưa Minh Nguyên';
--- site/config/site/vi/config_site.php
+++ site/config/site/vi/config_site.php
@@ -6,7 +6,9 @@
 $config['site_name'] = 'Công Ty TNHH Áo Mưa Minh Nguyên';
 $config['site_close'] = 0;
 $config['site_message_close'] = 'Website đang trong quá trình nâng. Vui lòng truy cập lại sau';
-$config['site_des'] = 'Chuyên sản xuất áo mưa. Áo mưa doanh nghiệp, áo mưa thời trang, áo mưa Covic 19, áo mưa trẻ em, áo đi mưa, áo mưa quảng cáo';
-$config['site_keyword'] = 'Áo mưa thời trang, áo mưa Covic 19, áo mưa trẻ em, áo đi mưa, áo mưa quảng cáo';
+$config['site_des'] = 'Chuyên sản xuất áo mưa. Áo mưa thời trang, áo mưa Covic 19, áo đi mưa, áo mưa quảng cáo, Áo mưa tặng phẩm, Áo mưa nhựa cao cấp,';
+$config['site_des'] = $config['site_des']. ' Áo mưa vải dù cao cấp, Áo mưa cánh dơi, Áo mưa bán lẻ, Áo mưa trẻ em, Áo mưa bộ vải dù, Áo mưa kiểu, Áo mưa bít, Áo mưa hai đầu';
+$config['site_keyword'] = 'Áo mưa thời trang, áo mưa Covic 19, áo đi mưa, áo mưa quảng cáo, Áo mưa tặng phẩm, Áo mưa nhựa cao cấp, Áo mưa vải dù cao cấp,';
+$config['site_keyword'] = $config['site_keyword'] .' Áo mưa cánh dơi, Áo mưa bán lẻ, Áo mưa trẻ em, Áo mưa bộ vải dù, Áo mưa kiểu, Áo mưa bít, Áo mưa hai đầu';
 
 /* End of file config_site*/
