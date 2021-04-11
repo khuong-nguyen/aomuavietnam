455a664e3eff3e22969204acc7f078cef615a94e
khuong-nguyen <khuongnv@nexlesoft.com>
2021-04-11 11:13:01 +0700

    tin tuc page

--- site/components/tintuc/views/detail.php
+++ site/components/tintuc/views/detail.php
@@ -1,6 +1,6 @@
 <div class="title-main">
 	<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?php echo base_url();?>" itemprop="url" class="homepage"><span itemprop="title">Trang chủ</span></a></div>
-	<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" class="no-bg"><a href="<?php echo site_url('tin-tuc');?>" itemprop="url"><span itemprop="title">Cẩm nang mua sắm</span></a></div>
+	<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" class="no-bg"><a href="<?php echo site_url('tin-tuc');?>" itemprop="url"><span itemprop="title">Tin tức</span></a></div>
 </div>
 <?php $this->load->view('/templates/default/html/banner_news');?>
 <div id="left-news">
