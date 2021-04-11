9d256d84e7d69ed17e013e073632bd0df4df5774
khuong-nguyen <khuongnv@nexlesoft.com>
2021-04-11 21:07:37 +0700

    update sitemap

--- site/views/sitemap.php
+++ site/views/sitemap.php
@@ -9,257 +9,257 @@
 
 <url>
   <loc>http://aomuavietnam.com/</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>1.00</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/gioi-thieu</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/tin-tuc</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/quy-trinh-dat-hang</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/ao-mua-tang-pham-1.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/ao-mua-quang-cao-5.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/ao-mua-thoi-trang-6.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/ao-mua-bo-vai-du-7.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/ao-mua-canh-doi-4.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/chuyen-muc/bao-bi-nhua-pvc-37.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-63.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-59.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-64.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-62.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-60.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
-  <priority>0.80</priority>
-</url>
-<url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-65.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-61.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-72.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-62.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
-  <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-77.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-59.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-65.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-63.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-tang-pham-64.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-70.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-72.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-52.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-73.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-77.html</loc>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-51.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-canh-doi-71.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.80</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/tin-tuc/lua-chon-ao-mua-an-toan-khi-tham-gia-giao-thong-43.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/tin-tuc/y-nghia-lich-su-ngay-83-45.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-1-84.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-2-85.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-3-86.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-4-87.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-5-88.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-quang-cao-minh-nguyen-style-6-89.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-78.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-style-1-79.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-style-2-80.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-style-3-81.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-style-4-82.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-thoi-trang-minh-nguyen-style-5-83.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-1-90.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-2-91.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-3-92.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-4-93.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-6-97.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/ao-mua-bo-vai-du-minh-nguyen-style-5-96.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-1-98.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-2-99.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-3-100.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-4-101.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-5-102.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/san-pham/bao-bi-nhua-pvc-minh-nguyen-style-6-103.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.64</priority>
 </url>
 <url>
   <loc>http://aomuavietnam.com/tin-tuc.html</loc>
-  <lastmod>2021-04-11T11:16:57+00:00</lastmod>
+  <lastmod>2021-04-11T14:04:55+00:00</lastmod>
   <priority>0.51</priority>
 </url>
 
